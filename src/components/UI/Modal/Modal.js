0c3b7c15ef56302a39d2e6ef6d8369af837020f0
juandeto <juandeto10@gmail.com>
2020-09-01 15:08:23 -0300

    changes in aux

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import classes from './Modal.module.css';
-import Aux from '../../../hoc/Aux';
+import Auxiliary from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component{
@@ -14,7 +14,7 @@ class Modal extends Component{
     }
     render(){
         return(
-        <Aux>
+        <Auxiliary>
         <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
         <div 
     className={classes.Modal}
@@ -24,7 +24,7 @@ class Modal extends Component{
     }}>
         {this.props.children}
     </div> 
-    </Aux>
+    </Auxiliary>
     )
     }
 }
